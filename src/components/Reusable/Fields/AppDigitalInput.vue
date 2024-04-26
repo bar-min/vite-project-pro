c7a33296171cc40a4af6a29dfa7e2601393df35a
Michael Barmin <bar.min.js@gmail.com>
2024-04-26 16:13:34 +0400

    add max option in digital-input

--- src/components/Reusable/Fields/AppDigitalInput.vue
+++ src/components/Reusable/Fields/AppDigitalInput.vue
@@ -2,7 +2,13 @@
 import { computed } from 'vue'
 
 const props = defineProps({
-  modelValue: { type: Number, required: true }
+  modelValue: {
+    type: [Number, String],
+    required: true
+  },
+  max: {
+    type: Number
+  }
 })
 
 const emit = defineEmits(['update:modelValue'])
@@ -12,20 +18,34 @@ const model = computed({
     return props.modelValue
   },
   set(value) {
+    if (value > props.max) {
+      emit('update:modelValue', props.max)
+      return
+    }
     emit('update:modelValue', value)
   }
 })
 
-function checkDigit(event) {
-  if (event.key.length === 1 && isNaN(Number(event.key))) {
-    event.preventDefault()
+// function checkDigit(event) {
+//   if (event.key.length === 1 && isNaN(Number(event.key))) {
+//     event.preventDefault()
+//   }
+// }
+
+function checkValue(e) {
+  if (e.target.value.match(/[^0-9]/g)) {
+    e.target.value = e.target.value.replace(/[^0-9]/g, '')
+  }
+
+  if (props.max) {
+    e.target.value = e.target.value > props.max ? props.max : e.target.value
   }
 }
 </script>
 
 <template>
   <div>
-    <input class="digital-input" inputmode="numeric" v-model.number="model" @keydown="checkDigit" />
+    <input class="digital-input" inputmode="numeric" v-model.number="model" @input="checkValue" />
   </div>
 </template>
 
