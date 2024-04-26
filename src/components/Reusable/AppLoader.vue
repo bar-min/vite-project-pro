9d00c39e1a2bb78f6ede714665664d59b5802846
Michael Barmin <bar.min.js@gmail.com>
2024-04-26 16:51:25 +0400

    add app-loader

---
+++ src/components/Reusable/AppLoader.vue
@@ -0,0 +1,39 @@
+<script setup></script>
+
+<template>
+  <div class="app-loader"></div>
+</template>
+
+<style lang="scss" scoped>
+.app-loader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 1000;
+}
+
+.app-loader::after {
+  content: '';
+  width: 60px;
+  height: 60px;
+  border: 5px solid #fff;
+  border-bottom-color: transparent;
+  border-radius: 50%;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
