3619123315f878feb73e1cbdc4b4b96980d657ef
Michael Barmin <bar.min.js@gmail.com>
2024-06-04 13:29:46 +0400

    change requests url

--- src/common/services/filters.js
+++ src/common/services/filters.js
@@ -2,14 +2,14 @@ import { http } from '../api'
 
 export async function getRegions(payload = { term: '', paging: null }) {
   payload.term = payload.term ?? ''
-  const { data } = await http.post('/gds/filter/regions', payload)
+  const { data } = await http.post('/filter/regions', payload)
 
   return data
 }
 
 export async function getCities(payload = { term: '', paging: null }) {
   payload.term = payload.term ?? ''
-  const { data } = await http.post('/gds/filter/cities', payload)
+  const { data } = await http.post('/filter/cities', payload)
 
   return data
 }
@@ -17,34 +17,34 @@ export async function getCities(payload = { term: '', paging: null }) {
 export async function getHotels(payload = { term: '', paging: null }) {
   payload.term = payload.term ?? ''
 
-  const { data } = await http.post('/gds/filter/hotels', payload)
+  const { data } = await http.post('/filter/hotels', payload)
 
   return data
 }
 
 export async function getCategories(payload = { term: '', paging: null }) {
   payload.term = payload.term ?? ''
-  const { data } = await http.post('/gds/filter/categories', payload)
+  const { data } = await http.post('/filter/categories', payload)
 
   return data
 }
 
 export async function getMeals(payload = { term: '', paging: null }) {
   payload.term = payload.term ?? ''
-  const { data } = await http.post('/gds/filter/meals', payload)
+  const { data } = await http.post('/filter/meals', payload)
 
   return data
 }
 
 export async function getContextItems(payload = { term: '', paging: null }) {
   payload.term = payload.term ?? ''
-  const { data } = await http.post('/gds/filter/context', payload)
+  const { data } = await http.post('/filter/context', payload)
 
   return data
 }
 
 export async function searchItems(payload) {
-  const { data } = await http.post('/gds/filter/search', payload, {
+  const { data } = await http.post('/filter/search', payload, {
     headers: {
       Authorization: `Basic ${btoa('iritest:test123')}`
     }
@@ -54,7 +54,7 @@ export async function searchItems(payload) {
 }
 
 export async function searchItemsByHotelId(id, payload) {
-  const { data } = await http.post(`/gds/filter/search/${id}`, payload, {
+  const { data } = await http.post(`/filter/search/${id}`, payload, {
     headers: {
       Authorization: `Basic ${btoa('iritest:test123')}`
     }
